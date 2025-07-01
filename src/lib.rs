bb84452144ae924841b839765d44fd6a337a9664
Alex Martens <alex@thinglab.org>
2025-07-01 07:57:58 -0700

    fix new clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,7 @@ impl From<rusb::Error> for Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
